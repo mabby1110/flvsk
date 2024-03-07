b6f8ebf261d637fae13a629c51f3741c931c3883
mabby <rznns.mabby@gmail.com>
2024-03-07 12:36:33 -0600

    hmm

--- flaskr/static/util.js
+++ flaskr/static/util.js
@@ -1,62 +1,6 @@
-async function fetchDevices() {
-    const response = await fetch("/api/get_devices");
-    const devices = await response.json();
-    console.log(devices);
-
-    const dispositivosContainer = document.querySelector('.dispositivos');
-
-    for (const branch in devices) {
-        const sucursalDiv = document.createElement('div');
-        sucursalDiv.setAttribute("id", 'id_'+branch)
-        sucursalDiv.classList.add('sucursal');
-
-        const branchHeader = document.createElement('h3');
-        branchHeader.textContent = branch;
-        sucursalDiv.appendChild(branchHeader);
-
-        for (const deviceType in devices[branch]) {
-            const tipoDispositivoDiv = document.createElement('div');
-            tipoDispositivoDiv.classList.add('tipo_dispositivo', 'hover');
-            tipoDispositivoDiv.setAttribute("id", 'id_'+branch+'_'+deviceType)
-
-            const deviceTypeHeader = document.createElement('p');
-            deviceTypeHeader.textContent = deviceType;
-            tipoDispositivoDiv.appendChild(deviceTypeHeader);
-
-            const hostList = document.createElement('ul');
-            tipoDispositivoDiv.appendChild(hostList);
-
-            for (const host in devices[branch][deviceType]) {
-                const hostItem = document.createElement('li');
-                hostItem.classList.add('dispositivo', 'hover');
-                hostItem.setAttribute("id", 'id_'+branch+'_'+deviceType+'_'+host)
-
-                if (devices[branch][deviceType][host]['conn']) {
-                    hostItem.classList.add('bgGreen');
-                } else {
-                    hostItem.classList.add('bgRed');
-                }
-
-                const hostName = document.createElement('p');
-                hostName.textContent = devices[branch][deviceType][host]['info']['host'];
-                hostItem.appendChild(hostName);
-
-                const hostIP = document.createElement('p');
-                hostIP.textContent = devices[branch][deviceType][host]['info']['ip'];
-                hostItem.appendChild(hostIP);
-
-                hostList.appendChild(hostItem);
-            }
-
-            sucursalDiv.appendChild(tipoDispositivoDiv);
-        }
-
-        dispositivosContainer.appendChild(sucursalDiv);
-    }
-}
-fetchDevices()
-
 let showDivA = true;
+let carrito = []
+
 function toggleView(event){
     event.preventDefault();
     const divA = document.getElementById('consola_global');
@@ -78,4 +22,14 @@ function loadText(obj, e, id_consola){
     }
 
     reader.readAsText(file);
+}
+
+function agregar(event) {
+    if (event.target.classList.contains('selected')) {
+        console.log(':p', event.target.id.split('_'));
+        event.target.classList.remove('selected');
+    } else {
+        console.log(':p', event.target.id);
+        event.target.classList.add('selected');
+    }
 }
